2802162d0baf898c712688a2da0ee19405469260
rowadalmadina2030 <rowadalmadina2030@gmail.com>
2025-10-05 15:42:49 +0300

    src file created

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+
+        public interface ICommandHandler<in ICommand> : ICommandHandler<ICommand>
+        
+
+    {
+
+    }
+    public interface ICommandHandler<in ICommand, TResponse> : IRequestHandler<ICommand, TResponse>
+        where ICommand : ICommand<TResponse> where TResponse : notnull
+    {
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name , List<string> Category , string Description 
+        , string ImageFile , decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult> // For logic of createProduct
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
